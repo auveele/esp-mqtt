66947deaf5f6302f9149faf9e0161a52412fd721
Nacho Soler <escribame@nachosoler.es>
2017-12-08 12:24:48 +0100

    Actulizo fichero de config

--- 01-persiana/sketch/config sample.ino
+++ 01-persiana/sketch/config sample.ino
@@ -1,54 +1,61 @@
 
-// ==========
-// config.ino
-//
-// Archivo para el respositorio, renombrar a 'config' para compilar.
-// El archivo config.ino se ignorará al hacer un commit para no
-// publicar las credenciales.
-//
-// Si este archivo es modificado,
-// eliminar del .gitignore para que se sincronice.
-// ==========
+/*########################
+##########################
+ 
+config.ino
 
+Archivo para el respositorio, renombrar a 'config' para compilar.
+El archivo config.ino se ignorará al hacer un commit para no
+publicar las credenciales.
 
-// ===========
+Si este archivo es modificado,
+eliminar del .gitignore para que se sincronice.
+
+##########################
+########################*/
+
+// ###########
 // DECLARE PIN
-// ===========
+// ###########
 #define UP_PIN 12
 #define DOWN_PIN 13
 #define UP_BUTTON 2
 #define DOWN_BUTTON 0
 
-// ============
+// ############
 // AUTO RESTART
-// ============
-#define H 6
+// ############
+#define H 1
 #define M 0
 #define S 0
+#define bad_restart_delay 600 // 1min
+// No tocables
 #define SES 60
 unsigned long RESTART_TIME = (H * SES * SES) + (M * SES) + S; // H * M * S
+byte conscient_restart = 0;
 
-// ===========
+// ###########
 // WIFI CONFIG
-// ===========
+// ###########
 #define WIFI_SSID "XXX"
 #define WIFI_PASS "XXX"
 #define WIFI_IP IPAddress(192, 168, 1, 101)
 #define WIFI_GATEWAY IPAddress(192, 168, 1, 1)
 #define WIFI_SUBNET IPAddress(255, 255, 255, 0)
 
-// ===========
+// ###########
 // MQTT SERVER
-// ===========
-#define MQTT_BROKER IPAddress(192, 168, 1, 6)
+// ###########
+#define MQTT_BROKER IPAddress(192, 168, 1, 13)
 #define MQTT_PORT 1883
-#define MQTT_USER "XXX"
-#define MQTT_PASS "XXX"
+#define MQTT_USER "domus"
+#define MQTT_PASS "xxxxx"
+// No tocables
+AsyncMqttClient mqttClient;
 
-// ===========
+// ###########
 // MQTT CLIENT
-// ===========
-
+// ###########
 String mqtt_slash = "/";
 String mqtt_scope = "DOMUS";
 String mqtt_room = "OFFICE";
@@ -58,22 +65,22 @@ String mqtt_client_id = "ESP8266-";         // This text is concatenated with Ch
 String mqtt_base_topic = mqtt_slash + mqtt_scope + mqtt_slash + mqtt_room + mqtt_slash + mqtt_device; // + mqtt_slash + "#";
 
 // Bloqueo del botón físico, LOW funciona todo, HIGH sólo MQTT
-boolean mqtt_only = LOW;
+int mqtt_only = 0;
 
-// ===========
+// ###########
 // PERSIANA
-// ===========
-
+// ###########
 #define INPUT_INTERVAL 100    // ms
 #define AUTO_INTERVAL 15      // segs
-#define SECURITY_DELAY 150   // ms
+#define SECURITY_DELAY 150    // ms
 
-//==========
+// ###########
 // PIN SETUP
-//==========
+// ###########
 void setup_pins() {
   pinMode(UP_PIN, OUTPUT);
   pinMode(DOWN_PIN, OUTPUT);
   pinMode(UP_BUTTON, INPUT);
   pinMode(DOWN_BUTTON, INPUT);
 }
+
